d08a0036667145806c462766dc878f2c2f991ce6
Sutolix <tg884ght@gmail.com>
2020-03-28 09:49:51 -0300

    reorganização no código e adição do layout base

--- src/App.js
+++ src/App.js
@@ -1,146 +1,116 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
-
-
+import './App.css';
 
 export default function App() {
-
-
-  const ddds =
-  [
-    {
-      'ddd':"011",
-      '016':1.90,
-      '017':1.70,
-      '018':0.90
-    },
-    {
-      'ddd':"016",
-      '011':2.90
-    },
-    {
-      'ddd':"017",
-      '011':2.70
-    },
-    {
-      'ddd':"018",
-      '011':1.90
-    },
-  ];
-
-
-  const plans = 
-  [
-    {fm: 30},
-    {fm: 60},
-    {fm: 120}
-  ];
-
-  const [plan, setPlan] = useState(30);
-  const [call, setCall] = useState(1);
-  const [minutes, setMinutes] = useState(1);
   const [planOn, setPlanOn] = useState('0');
   const [planOff, setPlanOff] = useState('0');
 
   const { register, handleSubmit } = useForm();
-  
+
   const onSubmit = data => {
-    teste(data)
+    calc(data)
   };
 
-  const [ddd] = [ddds];
-  const [planx] = [plans];
+  function calc(data) {
 
-    function teste (data) {
+    const cust = data.call;
+    var price = 0;
 
-      const cust = data.call;
-      var price = 0;
+    switch (cust) {
 
-    switch(cust) {
- 
       case '1':
-        price = 1.9;
+        price = '1.9';
         break;
-      
+
       case '2':
-        price = 1.7;
+        price = '1.7';
         break;
- 
+
       case '3':
-        price = 0.9;
+        price = '0.9';
         break;
- 
+
       case '4':
-        price = 2.9;
+        price = '2.9';
         break;
 
       case '5':
-        price = 2.7;
+        price = '2.7';
         break;
 
       case '6':
-        price = 1.9;
+        price = '1.9';
         break;
- 
+
       default:
         break;
-      }
-
-
-      const surplus = (data.minutes - data.plan);
-      var vprice = price*0.1;
-      console.log(data.minutes);
-      console.log(vprice);
-      if (surplus < 0){
-        setPlanOn('0');
-        setPlanOff((data.minutes) * price);
-      }else{
-        setPlanOn(surplus * vprice)
-        setPlanOff((data.minutes) * price);
-      }
     }
 
+    const surplus = (data.minutes - data.plan);
+    var fmprice = price * 0.1;
+
+    console.log(price);
+    console.log(surplus);
+
+    if (surplus < 0) {
+      setPlanOn('0');
+      setPlanOff(parseFloat(((data.minutes) * price).toFixed(2)));
+    } else {
+      setPlanOn(parseFloat(surplus * fmprice).toFixed(2));
+      setPlanOff(parseFloat((data.minutes) * price).toFixed(2));
+    }
+  }
+
   return (
-    <div>
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <div className="container d-flex justify-content-center">
+      <div className="calc row justify-content-around">
+        
+        <div className="form col-md-4">
+          <form id="plansform" onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label>Escolha um Plano</label>
+              <select className="form-control" ref={register} name="plan" id="plan" required>
+                <option value="30">FaleMais 30</option>
+                <option value="60">FaleMais 60</option>
+                <option value="120">FaleMais 120</option>
+              </select>
+            </div>
 
-      <label>Escolha um plano</label>
-      <select ref={register} name="plan">
-        <option value="30">FaleMais 30</option>
-        <option value="60">FaleMais 60</option>
-        <option value="120">FaleMais 120</option>
-      </select>
+            <div className="form-group">
+              <label>Selecione o DDD de origem</label>
+              <select className="form-control" ref={register} name="call" required>
+                <option value="1">011 a 016</option>
+                <option value="2">011 a 017</option>
+                <option value="3">011 a 018</option>
+                <option value="4">016 a 011</option>
+                <option value="5">017 a 011</option>
+                <option value="6">018 a 011</option>
+              </select>
+            </div>
 
-      <label>Ligação entre</label>
-      <select ref={register} name="call">
-        <option value="1">011 a 016</option>
-        <option value="2">011 a 017</option>
-        <option value="3">011 a 018</option>
-        <option value="4">016 a 011</option>
-        <option value="5">017 a 011</option>
-        <option value="6">018 a 011</option>
-      </select>
+            <div className="form-group">
+              <label htmlFor="minutes">Digite a duração da ligação</label>
+              <input className="form-control" ref={register} id="minutes" name="minutes" type="number" min="1" placeholder="minutos" required />
+            </div>
 
-      <label htmlFor="minutes">Duração</label>
-      <input type="number" min="1" name="minutes" placeholder="minutos" ref={register} />
+            <button className="btn btn-success align-self-center" type="submit">Ver por quanto fica</button>
+          </form>
+        </div>
 
-      <input type="submit" />
-    </form>
+        <div className="result col-md-4 d-flex justify-content-center align-items-center flex-column">
+          <div className="plan">
+            <p>Com FaleMais</p>
+            <span>${planOn}</span>
+          </div>
 
-
-
-    <div>
-
-    <p>Com plano</p>
-    <span>${planOn}</span>
-
-    <p>Sem plano</p>
-    <span>${planOff}</span>
-
-    </div>
-
-
+          <div className="plan">
+            <p>Sem FaleMais</p>
+            <span>${planOff}</span>
+          </div>
+        </div>
+      
+      </div>
     </div>
   )
 }
