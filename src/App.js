6262176e0947dedbc155b4a569d54a7bf3b490fb
Sutolix <tg884ght@gmail.com>
2020-03-29 19:41:00 -0300

    separação dos arrays de tarifas do código principal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { useAlert } from 'react-alert';
 import './global.css';
+import {prices, plans} from './services/tarifs.js';
 
 export default function App() {
   const [plan, setPlan] = useState('30');
@@ -8,54 +9,6 @@ export default function App() {
   const [minutes, setMinutes] = useState('');
   const [planOn, setPlanOn] = useState('0');
   const [planOff, setPlanOff] = useState('0');
-
-  const prices =[
-    {
-      title: "011 para 016",
-      call:"11-16",
-      price:"1.9"
-    },
-    {
-      title: "011 para 017",
-      call:"11-17",
-      price:"1.7"
-    },
-    {
-      title: "011 para 018",
-      call:"11-18",
-      price:"0.9"
-    },
-    {
-      title: "016 para 011",
-      call:"16-11",
-      price:"2.9"
-    },
-    {
-      title: "017 para 011",
-      call:"17-11",
-      price:"2.7"
-    },
-    {
-      title: "018 para 011",
-      call:"18-11",
-      price:"1.9"
-    }
-  ]
-
-  const plans = [
-    {
-      title: "FaleMais 30",
-      value: "30"
-    },
-    {
-      title: "FaleMais 60",
-      value: "60"
-    },
-    {
-      title: "FaleMais 120",
-      value: "120"
-    }
-  ]
 
   const alert = useAlert();
 
---
+++ src/services/tarifs.js
@@ -0,0 +1,47 @@
+export const prices =[
+    {
+      title: "011 para 016",
+      call:"11-16",
+      price:"1.9"
+    },
+    {
+      title: "011 para 017",
+      call:"11-17",
+      price:"1.7"
+    },
+    {
+      title: "011 para 018",
+      call:"11-18",
+      price:"0.9"
+    },
+    {
+      title: "016 para 011",
+      call:"16-11",
+      price:"2.9"
+    },
+    {
+      title: "017 para 011",
+      call:"17-11",
+      price:"2.7"
+    },
+    {
+      title: "018 para 011",
+      call:"18-11",
+      price:"1.9"
+    }
+  ]
+
+export const plans = [
+    {
+      title: "FaleMais 30",
+      value: "30"
+    },
+    {
+      title: "FaleMais 60",
+      value: "60"
+    },
+    {
+      title: "FaleMais 120",
+      value: "120"
+    }
+  ]
