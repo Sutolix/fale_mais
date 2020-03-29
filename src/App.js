1565b61e21865648a1d3d5c892c14f1fdfe8876b
Sutolix <tg884ght@gmail.com>
2020-03-29 13:27:10 -0300

    refatoração

--- src/App.js
+++ src/App.js
@@ -1,115 +1,120 @@
-import React, { useState } from "react";
+import React, {useState} from 'react';
 import { useAlert } from 'react-alert';
-import { useForm } from "react-hook-form";
 import './global.css';
 
 export default function App() {
+  const [plan, setPlan] = useState('30');
+  const [call, setCall] = useState('11-16');
   const [minutes, setMinutes] = useState('');
   const [planOn, setPlanOn] = useState('0');
   const [planOff, setPlanOff] = useState('0');
 
-  const { register, handleSubmit } = useForm();
-
   const alert = useAlert();
 
-  const onSubmit = data => {
-    calc(data)
-  };
-
-  function calc(data) {
-
-    const cust = data.call;
-    var price = 0;
-
-    switch (cust) {
-
-      case '1':
-        price = '1.9';
-        break;
-
-      case '2':
-        price = '1.7';
-        break;
-
-      case '3':
-        price = '0.9';
-        break;
-
-      case '4':
-        price = '2.9';
-        break;
-
-      case '5':
-        price = '2.7';
-        break;
-
-      case '6':
-        price = '1.9';
-        break;
-
-      default:
-        break;
+  const prices =[
+    {
+      call:"11-16",
+      price:"1.9"
+    },
+    {
+      call:"11-17",
+      price:"1.7"
+    },
+    {
+      call:"11-18",
+      price:"0.9"
+    },
+    {
+      call:"16-11",
+      price:"2.9"
+    },
+    {
+      call:"17-11",
+      price:"2.7"
+    },
+    {
+      call:"18-11",
+      price:"1.9"
     }
+  ]
 
-    const surplus = (data.minutes - data.plan);
-    var fmprice = price * 0.1;
-    var validminutes = `${data.minutes}`.length;
+  function handleCalc(e){
+    e.preventDefault();
 
-    console.log(price);
-    console.log(surplus);
-    console.log(validminutes);
+    let find = (prices.filter(item => item.call === call ))[0];
+    let price = find.price;
+    let fmprice = ((parseFloat(price)* 0.1) + parseFloat(price));
+    let validminutes = `${minutes}`.length;
+    let surplus = (minutes - plan);
 
-    if ((data.minutes <= 0) || (validminutes >= 5)){
+    if ((minutes <= 0) || (validminutes >= 5)){
       alert.error('Por favor, digite um valor válido!');
       setPlanOn('0');
       setPlanOff('0');
       setMinutes('');
-    
-    } else if ((surplus < 0) & (data.minutes >= 1)) {
+    } else if ((surplus < 0) & (minutes >= 1)) {
       setPlanOn('0');
-      setPlanOff(parseFloat(((data.minutes) * price).toFixed(2)));
+      setPlanOff(parseFloat(((minutes) * price).toFixed(2)));
     } else {
       setPlanOn(parseFloat(surplus * fmprice).toFixed(2));
-      setPlanOff(parseFloat((data.minutes) * price).toFixed(2));
+      setPlanOff(parseFloat(minutes * price).toFixed(2));
     }
   }
 
   return (
     <div className="container d-flex flex-column">
+
       <div className="head">
         <span>FaleMais</span>
         <span>Telzir</span>
       </div>
-      <div className="calc row justify-content-around align-items-center">
-        
+
+      <section className="calc row justify-content-around align-items-center">
         <div className="form col-md-4 order-md-1 order-12">
-          <form id="plansform" onSubmit={handleSubmit(onSubmit)}>
+          <form id="plansform" onSubmit={handleCalc}>
             <div className="form-group">
               <label>Escolha um Plano</label>
-              <select className="form-control" ref={register} name="plan" id="plan" required>
+              <select
+                className="form-control"
+                name="plan"
+                required
+                value={plan}
+                onChange={e => setPlan(e.target.value)}
+              >
                 <option value="30">FaleMais 30</option>
                 <option value="60">FaleMais 60</option>
                 <option value="120">FaleMais 120</option>
               </select>
             </div>
-
             <div className="form-group">
-              <label>Selecione os ddds</label>
-              <select className="form-control" ref={register} name="call" required>
-                <option value="1">011 a 016</option>
-                <option value="2">011 a 017</option>
-                <option value="3">011 a 018</option>
-                <option value="4">016 a 011</option>
-                <option value="5">017 a 011</option>
-                <option value="6">018 a 011</option>
+              <label>Selecione os ddds da ligação</label>
+              <select
+                className="form-control"
+                name="call"
+                required
+                value={call}
+                onChange={e => setCall(e.target.value)}
+              >
+                <option value="11-16">011 para 016</option>
+                <option value="11-17">011 para 017</option>
+                <option value="11-18">011 para 018</option>
+                <option value="16-11">016 para 011</option>
+                <option value="17-11">017 para 011</option>
+                <option value="18-11">018 para 011</option>
               </select>
             </div>
-
             <div className="form-group">
               <label htmlFor="minutes">Duração da chamada</label>
-              <input className="form-control" ref={register} id="minutes" name="minutes" type="number" min="1" placeholder="min" required value={minutes} onChange={e => setMinutes(e.target.value)}/>
+              <input
+                className="form-control"
+                name="minutes"
+                type="number"
+                min="1"
+                placeholder="min"
+                required
+                value={minutes}
+                onChange={e => setMinutes(e.target.value)}/>
             </div>
-
             <button className="btn btn-success align-self-center" type="submit">Ver por quanto fica</button>
           </form>
         </div>
@@ -127,7 +132,7 @@ export default function App() {
             <span>${planOff}</span>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   )
 }
